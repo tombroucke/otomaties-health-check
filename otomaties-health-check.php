608efdf3fba610b6fd2f6f1a95bb5979491fc96b
Tom Broucke <tom@tombroucke.be>
2024-05-31 11:59:35 +0200

    🔼 chore(bump): Bump version to 1.2.1

--- otomaties-health-check.php
+++ otomaties-health-check.php
@@ -8,7 +8,7 @@ use Otomaties\HealthCheck\Helpers\Loader;
 /**
  * Plugin Name:       Otomaties Health Check
  * Description:       Add a health check to check for common issues
- * Version:           1.2.0
+ * Version:           1.2.1
  * Author:            Tom Broucke
  * Author URI:        https://tombroucke.be/
  * License:           GPL-2.0+
